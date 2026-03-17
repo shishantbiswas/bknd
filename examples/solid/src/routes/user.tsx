b2315ed0129f1435a47fb9cbed46972613c56a14
Shishant Biswas <shishantbiswas@gmail.com>
2026-03-18 03:30:10 +0530

    refactor: dark mode styling

--- examples/solid/src/routes/user.tsx
+++ examples/solid/src/routes/user.tsx
@@ -45,7 +45,7 @@ export default function Home() {
       <main class="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div class="flex flex-row items-center justify-evenly min-w-full">
           <img
-            class="dark:invert size-18"
+            class="size-18"
             src="/solid.svg"
             alt="Solid logo"
           />
@@ -112,7 +112,7 @@ function Buttons() {
   return (
     <div class="flex gap-4 items-center flex-col sm:flex-row">
       <a
-        class="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground gap-2 text-white hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+        class="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground gap-2 text-white hover:bg-[#383838] dark:hover:bg-[#ccc] dark:text-black text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
         href="https://bknd.io/"
         target="_blank"
         rel="noopener noreferrer"
